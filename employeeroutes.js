6d20a954d43381bab5b3172e287ac3dfc49e09fb
Pranav Kiran Mahale <pranav@Pranavs-MacBook-Air.local>
2025-02-22 10:30:41 +0530

    Get employee Updated

--- employeeroutes.js
+++ employeeroutes.js
@@ -28,6 +28,7 @@ router.get("/employees/:qr_code", async (req, res) => {
     const { qr_code } = req.params;
 
     try {
+        console.log("Fetching employee with QR code:", qr_code);
         const [employee] = await db.query("SELECT * FROM Users WHERE qr_code = ?", [qr_code]);
 
         if (employee.length === 0) {
